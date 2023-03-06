b8e3aee83ea37e780d7f3c4d489d2f3755f1112d
Alexandre Arturi <alexandrea8@nycstudents.net>
2023-03-06 12:26:44 -0500

    3-6

--- src/String2ArrayList.java
+++ src/String2ArrayList.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class String2ArrayList {
+import java.util.ArrayList;
+public class String2ArrayList
+{
+    private ArrayList<String> splitString;
+
+    public String2ArrayList()
+    {
+        splitString = new ArrayList<String>();
+    }
+    public void splitAndAdd(String str)
+    {
+        while(str.length()>0){
+            splitString.add(str.substring(0,1));
+            str = str.substring(1);
+        }
+    }
+    public String toString()
+    {
+        return splitString.toString();
+    }
 }
